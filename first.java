3ee4a8eaab9bc4b129f691637dde587a0201ea5a
Rajeev Ranjan <159820641+RajeevRanjansing@users.noreply.github.com>
2024-07-23 22:42:31 +0530

    first.java

---
+++ first.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class first{
+    public static void main(String args[]){
+
+        //Type safe means only one type of object either String or integer 
+        ArrayList<String> name=new ArrayList<String>();
+        name.add("Rajeev");
+        name.add("Akash");
+        name.add("Amit");
+        name.add("Rajeev");
+        
+      System.out.println(name);            //output=[Rajeev,Akash,Amit,Rajeev]
+      System.out.println(name.remove("Rajeev"));      //output=[Akash,Amit]
+        //un-type safe means we can put string and integer etc
+    }
+}
