590ec4e53774c92b89cb2c6c9ca508aab0e84869
Rajeev Ranjan <159820641+RajeevRanjansing@users.noreply.github.com>
2024-07-23 22:50:06 +0530

    first.java

--- first.java
+++ first.java
@@ -1,3 +1,5 @@
+//first program
+
 import java.util.*;
 class first{
     public static void main(String args[]){
@@ -11,6 +13,25 @@ class first{
         
       System.out.println(name);            //output=[Rajeev,Akash,Amit,Rajeev]
       System.out.println(name.remove("Rajeev"));      //output=[Akash,Amit,Rajeev]
+        
         //un-type safe means we can put string and integer etc
     }
+}
+
+//Second program
+
+
+import java.util.*;
+class first{
+    public static void main(String args[]){
+
+        ArrayList<String> name=new ArrayList<String>();
+        name.add("Rajeev");
+        name.add("Akash");
+        name.add("Amit");
+        name.add("Rajeev");
+        
+      System.out.println(name.size());     //output=4           
+        
+     }
 }
